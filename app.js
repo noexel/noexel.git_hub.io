66d68b7821a01e62a78bcdb7d216c89c457859ee
noexel <101858193+noexel@users.noreply.github.com>
2024-04-30 19:41:35 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = 'FFFFFF';
+tg.MainButtonc.color = '#1676E7';
+
+tg.MainButton.show()
+tg.MainButton.setText('Добро пожаловать!');
+
+let usercard = document.getElementById('usercard');
+
+let p = document.createElement('p');
+
+p.innerText = `${tg.initDataUnsafe.first_name}
+${tg.initDataUnsafe.last_name}`
+
+usercard.appendChild(p);
