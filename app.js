5a94d5de0f76eea0b6b3644739a6d32be293a786
noexel <101858193+noexel@users.noreply.github.com>
2024-04-30 19:48:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ let usercard = document.getElementById('usercard');
 
 let p = document.createElement('p');
 
-p.innerText = `${tg.initDataUnsafe.first_name}
-${tg.initDataUnsafe.last_name}`;
+p.innerText = `${tg.initDataUnsafe.first_name} ${tg.initDataUnsafe.last_name}`;
 
 usercard.appendChild(p);
