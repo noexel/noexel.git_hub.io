4f81cd5083429dc03da9eb64e0bc319d8adf8b57
noexel <101858193+noexel@users.noreply.github.com>
2024-04-30 20:01:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
-
-let usercard = document.getElementById('usercard');
-
-let p = document.createElement('p');
-
-p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
